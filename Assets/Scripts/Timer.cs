58b2f7be04a50c30ffab6a49adc51de9780cec07
rgo555 <95858482+Edgarduba@users.noreply.github.com>
2023-12-01 13:09:26 +0100

    me rindo

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,32 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.Text;
-//using UnityEngineTMP;
+using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
-/*public Text textTimer;
-public float timer = 5;
-void Start();
-{
-
-}
-
-void Update()
-{
-if(timer < 0)
-    {
-    SceneManager.LoadScene(1);
-    }
-
-timer -= Time.deltaTime;
-textTimer.text = "" + timer.ToString("f1");
-}
-
-*/
-
 void Start()
     {
+        //public Text text;
         StartCoroutine(ExampleCoroutine());
     }
 
